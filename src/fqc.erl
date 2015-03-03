6394a557c66edf9d5f5df6df78e4cb32197f7374
Heinz N. Gies <heinz@licenser.net>
2015-03-03 15:44:26 +0100

    Some more exports.

--- src/fqc.erl
+++ src/fqc.erl
@@ -5,13 +5,14 @@
 
 -include_lib("eqc/include/eqc.hrl").
 
--export([not_empty/1, maybe_oneof/2, non_blank_string/0, lower_char/0]).
+-export([not_empty/1, maybe_oneof/2, non_blank_string/0, lower_char/0,
+         pos_integer/0, non_neg_integer/0]).
 
 not_empty(G) ->
     ?SUCHTHAT(X, G, X /= [] andalso X /= <<>>).
 
 non_blank_string() ->
-    not_empty(?LET(X,list(lower_char()), list_to_binary(X))).
+    non_empty(?LET(X,list(lower_char()), list_to_binary(X))).
 
 %% Generate a lower 7-bit ACSII character that should not cause any problems
 %% with utf8 conversion.
@@ -21,5 +22,11 @@ lower_char() ->
 
 maybe_oneof(L, T) ->
     ?LET(E, ?SUCHTHAT(E, T, not lists:member(E, L)), oneof([E | L])).
+
+pos_integer() ->
+    ?SUCHTHAT(X, int(), X >= 0).
+
+non_neg_integer() ->
+    ?SUCHTHAT(X, int(), X > 0).
 
 -endif.
